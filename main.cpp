fbd26869122b3642abf5a21d267c32fe43446d54
Tanya199901 <marushka.tanya@mail.ru>
2021-05-17 11:35:25 -0700

    Log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTextStream>
+#include <QScopedPointer>
+
+QScopedPointer<QFile> m_logFile;
+
+void messageHanlder(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    m_logFile.reset(new QFile("logFile.txt"));
+    m_logFile.data()->open(QFile::Append | QFile::Text);
+    qInstallMessageHandler(messageHanlder);
+
     MainWindow w;
     w.show();
     return a.exec();
+}
+
+void messageHanlder(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(context);
+    QTextStream out(m_logFile.data());
+    out << msg << endl;
+    out.flush();
 }
